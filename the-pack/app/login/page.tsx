b7a3d68c73099c2e3fdf77123e57b72b456653ba
tylermong <117101211+tylermong@users.noreply.github.com>
2024-10-03 01:03:31 -0400

    Changed login page styling

--- the-pack/app/login/page.tsx
+++ the-pack/app/login/page.tsx
@@ -4,81 +4,73 @@ import Image from "next/image";
 
 export default function Login(){
     return(
-
         <div>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <Image
+                        src="/The Pack Logo.jpg"
+                        width={500}
+                        height={500}
+                        draggable={false}
+                        alt="The Pack Logo"
+                    />
 
-                <Image
-                    src = "/The Pack Logo.jpg"
-                    width = {500}
-                    height = {500}
-                    alt = "The Pack Logo"
-                />
-
-                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
-                    Sign in to your account
-                </h2>
-
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+                        Sign in
+                    </h2>
                 </div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form action="#" method="POST" className="space-y-6">
-                    <div>
-                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
-                        Email address
-                    </label>
-                    <div className="mt-2">
-                        <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        required
-                        autoComplete="email"
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-900 sm:text-sm sm:leading-6"
-                        />
-                    </div>
-                    </div>
-
-                    <div>
-                    <div className="flex items-center justify-between">
-                        <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
-                        Password
-                        </label>
-                        <div className="text-sm">
-                        <a href="#" className="font-semibold text-white hover:text-gray-400">
-                            Forgot password?
-                        </a>
+                    <form action="#" method="POST" className="space-y-6">
+                        <div className="relative">
+                            <input
+                            id="email"
+                            name="email"
+                            type="email"
+                            required
+                            autoComplete="email"
+                            className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-white bg-black rounded-lg border border-white focus:outline-none focus:ring-0 peer" placeholder=""
+                            />
+                            
+                            <label
+                            htmlFor="email"
+                            className="absolute text-sm text-white duration-300 transform select-none cursor-text -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-black px-2 peer-focus:px-2 peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">
+                                Email
+                            </label>
                         </div>
-                    </div>
-                    <div className="mt-2">
-                        <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        required
-                        autoComplete="current-password"
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-900 sm:text-sm sm:leading-6"
-                        />
-                    </div>
-                    </div>
 
-                    <div>
-                    <button
-                        type="submit"
-                        className="flex w-full justify-center rounded-md bg-gray-400 px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
-                    >
-                        Sign in
-                    </button>
-                    </div>
-                </form>
+                        <div className="relative">
+                            <input
+                            id="password"
+                            name="password"
+                            type="password"
+                            required
+                            autoComplete="current-password"
+                            className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-white bg-black rounded-lg border border-white focus:outline-none focus:ring-0 peer" placeholder=""
+                            />
+                            
+                            <label
+                            htmlFor="password"
+                            className="absolute text-sm text-white duration-300 transform select-none cursor-text -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-black px-2 peer-focus:px-2 peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">
+                                Password
+                            </label>
+                        </div>
 
-                <p className="mt-10 text-center text-sm text-gray-500">
-                    Not a member?{' '}
-                    <a href="#" className="font-semibold leading-6 text-gray-400 hover:text-gray-600">
-                    Register
-                    </a>
-                </p>
+                        <div>
+                            <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-white px-3 py-1.5 m-0 text-sm font-semibold leading-6 text-black shadow-sm hover:bg-gray-300 transition duration-300">
+                                Sign in
+                            </button>
+                        </div>
+                    </form>
+
+                    <p className="mt-10 text-center text-sm white">
+                        Not a member?{' '}
+                        <a href="#" className="font-bold leading-6 text-white hover:text-gray-300 transition duration-300">
+                            Register
+                        </a>
+                    </p>
                 </div>
             </div>
         </div>
