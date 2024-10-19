a7a9b2bd196e2c13971c1da4a7b85c9737d15829
Cachary Tolentino <tolenti2@go.stockton.edu>
2024-10-19 16:00:20 -0400

    quick change

--- the-pack/src/components/Jumbotron.tsx
+++ the-pack/src/components/Jumbotron.tsx
@@ -23,7 +23,7 @@ const Jumbotron = () =>{
                         Login
 
                         <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                         </svg>
 
                     </a>
