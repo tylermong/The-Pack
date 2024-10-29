fac152fcc9eff53365e4f37f9f4cb7309a863ba5
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2024-10-29 12:15:40 -0400

    Delete the-pack-backend/src directory

--- the-pack-backend/src/announcements/dto/update-announcements.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAnnouncementsDto } from './create-announcements.dto';
-
-export class UpdateAnnouncementsDto extends PartialType(CreateAnnouncementsDto) {}
