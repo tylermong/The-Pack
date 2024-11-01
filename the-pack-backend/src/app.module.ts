a8783a311891437c0f7190fd4de0f928face0338
Raghvandersinh <89419757+Raghvandersinh@users.noreply.github.com>
2024-11-01 15:46:59 -0400

    Created an create, delete (empty class), modify class, find all classes and find single classes. For the classes module

--- the-pack-backend/src/app.module.ts
+++ the-pack-backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { CoachModule } from './coach/coach.module';
 import { AnnouncementsModule } from './announcements/announcements.module';
+import { ClassesModule } from './classes/classes.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, CoachModule, AnnouncementsModule],
+  imports: [PrismaModule, UserModule, CoachModule, AnnouncementsModule, ClassesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- the-pack-backend/src/classes/classes.controller.ts
+++ the-pack-backend/src/classes/classes.controller.ts
@@ -7,8 +7,32 @@ import { Prisma } from '@prisma/client';
 export class ClassesController {
     constructor(private readonly classesService: classesService) {}
 
+    
+
+    @Post()
+    async create(@Body() { creatorId, ...classData }: { creatorId: string } & Prisma.classesCreateInput) {
+        return this.classesService.createClass(creatorId, classData);
+    }
+    @Get()
+    async getClasses() {
+        return this.classesService.getAllClasses();
+    }
+    @Get(':id')
+    async getClassById(@Param('id') id: string) {
+        return this.classesService.getClassById(id);
+    }
+
     @Post('join')
     async joinClass(@Body('userId') userId: string, @Body('classId') classId: string): Promise<string> {
     return this.classesService.joinClass(userId, classId);
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') classId: string) {
+        return this.classesService.deleteClass(classId);
+    }
+    @Patch(':id')
+    async update(@Param('id') classId: string, @Body() classData: Prisma.classesUpdateInput) {
+        return this.classesService.updateClass(classId, classData);
     }
 }
