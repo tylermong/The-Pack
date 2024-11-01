07bfab8fa148c7c4bb9afb5f3df4c4e35f09d7df
Melvin <Melvinvazquez16@yahoo.com>
2024-10-31 23:06:01 -0400

    Added a limit to classes

--- the-pack-backend/src/classes/classes.service.ts
+++ the-pack-backend/src/classes/classes.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { SrvRecord } from 'dns';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -10,6 +10,15 @@ export class classesService {
     constructor(private prisma: PrismaService){}
 
     async joinClass(userId: string, classId: string): Promise<string>{
+        const enrollmentCount = await this.prisma.userClasses.count({
+            where: { classId: classId },
+          });
+      
+          // Restrict enrollment if the class is full (8 users)
+          if (enrollmentCount >= 8) {
+            throw new ConflictException('Class enrollment is full');
+          }
+
         const existingEnrollment = await this.prisma.userClasses.findUnique({
             where:{
                 clientId_classId: {clientId: userId, classId: classId},
