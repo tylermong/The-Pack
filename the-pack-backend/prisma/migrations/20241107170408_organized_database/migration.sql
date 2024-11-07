8c1552418f96491a52c14793feccf360e1ba4deb
Melvin <Melvinvazquez16@yahoo.com>
2024-11-07 12:04:23 -0500

    Migrated changes into the database

---
+++ the-pack-backend/prisma/migrations/20241107170408_organized_database/migration.sql
@@ -0,0 +1,110 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `usersCoachid` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the `Coach` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `classes` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "ExerciseIntensity" AS ENUM ('LOW', 'MEDIUM', 'HIGH');
+
+-- CreateEnum
+CREATE TYPE "MealType" AS ENUM ('BREAKFAST', 'LUNCH', 'DINNER', 'SNACK');
+
+-- DropForeignKey
+ALTER TABLE "User" DROP CONSTRAINT "User_usersCoachid_fkey";
+
+-- DropForeignKey
+ALTER TABLE "announcements" DROP CONSTRAINT "announcements_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "classes" DROP CONSTRAINT "classes_creatorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "scheduling" DROP CONSTRAINT "scheduling_coachId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "userClasses" DROP CONSTRAINT "userClasses_classId_fkey";
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "usersCoachid",
+ADD COLUMN     "coachId" TEXT;
+
+-- DropTable
+DROP TABLE "Coach";
+
+-- DropTable
+DROP TABLE "classes";
+
+-- CreateTable
+CREATE TABLE "Class" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "creatorId" TEXT NOT NULL,
+    "currentlyEnrolled" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Class_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "NutritionTracker" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "userId" TEXT NOT NULL,
+    "timeSlot" TEXT,
+    "calories" INTEGER,
+    "protein" DOUBLE PRECISION,
+    "carbohydrates" DOUBLE PRECISION,
+    "fats" DOUBLE PRECISION,
+    "notes" TEXT,
+    "mealType" "MealType",
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "NutritionTracker_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ExerciseTracker" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "userId" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "timeSlot" TEXT,
+    "duration" INTEGER,
+    "intensity" "ExerciseIntensity",
+    "caloriesBurned" INTEGER,
+    "exerciseType" TEXT,
+    "notes" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ExerciseTracker_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_coachId_fkey" FOREIGN KEY ("coachId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "announcements" ADD CONSTRAINT "announcements_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Class" ADD CONSTRAINT "Class_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "userClasses" ADD CONSTRAINT "userClasses_classId_fkey" FOREIGN KEY ("classId") REFERENCES "Class"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "scheduling" ADD CONSTRAINT "scheduling_coachId_fkey" FOREIGN KEY ("coachId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "NutritionTracker" ADD CONSTRAINT "NutritionTracker_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ExerciseTracker" ADD CONSTRAINT "ExerciseTracker_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
