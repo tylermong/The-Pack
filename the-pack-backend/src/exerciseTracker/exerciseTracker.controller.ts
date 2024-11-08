066fd0b849bd718f233630ba5f17fe35ac797f1a
Raghvandersinh <89419757+Raghvandersinh@users.noreply.github.com>
2024-11-08 17:30:51 -0500

    Created a function that allows the user to create they Exercise Entry and also a dto that validates the data entered.

--- the-pack-backend/src/exerciseTracker/exerciseTracker.controller.ts
+++ the-pack-backend/src/exerciseTracker/exerciseTracker.controller.ts
@@ -1,8 +1,14 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { ExerciseTrackerService } from './exerciseTracker.service';
 import { Prisma } from '@prisma/client';
+import { CreateExerciseTrackerDto } from './dtos/CreateExerciseTracker.dto';
 
 @Controller('exerciseTracker')
 export class ExerciseTrackerController{
     constructor(private readonly exerciseTrackerService:ExerciseTrackerService){}
+    
+    @Post()
+    async createExercise(@Body() CreateExerciseTrackerDto: CreateExerciseTrackerDto){
+        return await this.exerciseTrackerService.createUserExerciseEntry(CreateExerciseTrackerDto)
+    }
 }
--- the-pack-backend/src/exerciseTracker/exerciseTracker.service.ts
+++ the-pack-backend/src/exerciseTracker/exerciseTracker.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from "@nestjs/common";
 import { Prisma } from "@prisma/client";
 import { PrismaService } from "src/prisma/prisma.service";
+import { CreateExerciseTrackerDto } from "./dtos/CreateExerciseTracker.dto";
 
 
 @Injectable()
 export class ExerciseTrackerService{
     constructor(private prisma:PrismaService){}
+
+    async createUserExerciseEntry(data: CreateExerciseTrackerDto ){
+        const {userId, ...rest} = data;
+        
+        return await this.prisma.exerciseTracker.create({
+            data: {
+                ...rest,
+    
+                user:{
+                    connect: {id:userId}
+                },
+                date: new Date()
+            }
+        })
+    }
+
 }
