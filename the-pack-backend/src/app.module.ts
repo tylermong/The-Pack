02b79a7b639fec61128f73cf7b62700f2d5bacf0
Raghvandersinh <89419757+Raghvandersinh@users.noreply.github.com>
2024-11-08 14:57:25 -0500

    Had two imports in the app.module when I was merging, I removed the out of date one.

--- the-pack-backend/src/app.module.ts
+++ the-pack-backend/src/app.module.ts
@@ -12,17 +12,15 @@ import { userClassesModule } from './userClasses/userClasses.module';
 import { schedulingModule } from './scheduling/scheduling.module';
 import { ExerciseTrackerModule } from './userExerciseTracker/userExerciseTracker.module';
 import { NutritionTrackerModule } from './userNutritionTracker/userNutritionTracker.module';
-import { CoachNutritionTrackerModule } from './coachNutritionTracker/coachNutritionTracker.module';
 import { ConfigModule } from '@nestjs/config';
 import { ChatroomModule } from './chatroom/chatroom.module';
 import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, CoachModule, 
-    AnnouncementsModule, ClassesModule, userClassesModule, schedulingModule, AuthModule, 
-    NutritionTrackerModule, ExerciseTrackerModule, CoachNutritionTrackerModule],
-  imports: [ConfigModule.forRoot({isGlobal: true}) ,PrismaModule, UserModule, CoachModule, 
-    AnnouncementsModule, ClassModule, userClassesModule, schedulingModule, AuthModule, ChatroomModule, MessagesModule],
+  
+  imports: [ConfigModule.forRoot({isGlobal: true}),PrismaModule, UserModule, CoachModule, 
+    AnnouncementsModule, ClassModule, userClassesModule, schedulingModule, 
+    AuthModule, ChatroomModule, MessagesModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
