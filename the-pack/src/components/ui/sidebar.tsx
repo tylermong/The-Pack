387b9517652a6ece973aeea4418a315bdfbe871f
tylermong <117101211+tylermong@users.noreply.github.com>
2024-11-09 15:17:39 -0500

    Fixed mobile sidebar visiblity issue

--- the-pack/src/components/ui/sidebar.tsx
+++ the-pack/src/components/ui/sidebar.tsx
@@ -199,7 +199,7 @@ const Sidebar = React.forwardRef<
           <SheetContent
             data-sidebar="sidebar"
             data-mobile="true"
-            className="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
+            className="w-[--sidebar-width] bg-primary p-0 text-sidebar-foreground [&>button]:hidden"
             style={
               {
                 "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
