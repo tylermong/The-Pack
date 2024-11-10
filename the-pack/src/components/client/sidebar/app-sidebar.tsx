62359a6670126e188e4269ffb2ea18a217b38a26
tylermong <117101211+tylermong@users.noreply.github.com>
2024-11-09 19:05:16 -0500

    Update sidebar label from 'User' to 'Client' for clarity

--- the-pack/src/components/client/sidebar/app-sidebar.tsx
+++ the-pack/src/components/client/sidebar/app-sidebar.tsx
@@ -45,7 +45,7 @@ export function AppSidebar() {
     <Sidebar>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>The Pack • User</SidebarGroupLabel>
+          <SidebarGroupLabel>The Pack • Client</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
