1996c9be31bafc56ad2f61030e14d0b349eb495f
Cachary Tolentino <tolenti2@go.stockton.edu>
2024-11-13 09:52:07 -0500

    FOR MILESTONE#3 CLASS DEMO DO NOT TOUCH

--- the-pack/src/components/Jumbotron.tsx
+++ the-pack/src/components/Jumbotron.tsx
@@ -19,7 +19,7 @@ const Jumbotron = () =>{
                 <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
                     <a 
                     className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-black rounded-lg bg-white hover:bg-gray-300 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
-                    onClick={() => router.push("./clientlogin")}>
+                    onClick={() => router.push("/clientlogin")}>
                         Login
 
                         <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -30,7 +30,7 @@ const Jumbotron = () =>{
 
                     <a 
                     className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
-                    onClick={() => router.push("./clientregister")}>
+                    onClick={() => router.push("/clientregister")}>
                         Register
                     </a>  
 
