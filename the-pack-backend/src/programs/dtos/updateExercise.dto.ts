5df654c60e3af19edb0fb5686d98f3f438915ee6
Raghvandersinh <89419757+Raghvandersinh@users.noreply.github.com>
2024-11-15 22:01:04 -0500

    Removed UUID parameters in the update dtos

--- the-pack-backend/src/programs/dtos/updateExercise.dto.ts
+++ the-pack-backend/src/programs/dtos/updateExercise.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsNotEmpty, IsUUID, IsOptional } from 'class-validator';
+import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class UpdateExerciseDto {
   @IsNotEmpty()
--- the-pack-backend/src/programs/dtos/updateProgram.dto.ts
+++ the-pack-backend/src/programs/dtos/updateProgram.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsOptional, IsUUID } from 'class-validator';
+import { IsString, IsOptional} from 'class-validator';
 
 export class UpdateProgramDto {
   programId: string;  
