ffed1d95f6d27284cd43a9fb018b623c92dd8673
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2024-11-17 18:08:50 -0500

    feat: add CoachKeyService to AdminModule providers

--- the-pack-backend/src/admin/admin.module.ts
+++ the-pack-backend/src/admin/admin.module.ts
@@ -5,9 +5,10 @@ import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CoachService } from 'src/coach/coach.service';
 import { UserService } from 'src/user/user.service';
+import { CoachKeyService } from 'src/coach-key/coach-key.service';
 
 @Module({
   controllers: [AdminController],
-  providers: [AdminService, JwtService, PrismaService, CoachService, UserService],
+  providers: [AdminService, JwtService, PrismaService, CoachService, CoachKeyService, UserService],
 })
 export class AdminModule {}
